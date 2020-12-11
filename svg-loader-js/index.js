fa622bd19b2ba63c4f26fe6cafb7ad8968e7fd8d
jexists <jexists486@gmail.com>
2020-12-11 12:48:42 +0900

    backup

--- svg-loader-js/index.js
+++ svg-loader-js/index.js
@@ -6,10 +6,6 @@ const donut = document.querySelector('.loading_circle');
 const degree = 3.6 * (100 - parseInt(donut.getAttribute('data-per')));
 const r = parseInt(donut.getAttribute('r'));
 
-// console.log(donut.getAttribute('data-per'));
-// console.log(r);
-
-
 document.documentElement.style.setProperty('--l', r * 3.141592 * 2);
 document.documentElement.style.setProperty('--deg', degree.toString() + "deg");
 document.documentElement.style.setProperty('--result', 2 * 3.141592 * r * (degree / 360));
