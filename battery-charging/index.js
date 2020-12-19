83dd32390377ccd8886f2727ebb0ba0590ca76aa
jexists <jexists486@gmail.com>
2020-12-20 00:11:54 +0900

    battery charging

---
+++ battery-charging/index.js
@@ -0,0 +1,27 @@
+console.log('index.js')
+
+
+const donut = document.querySelector('.loading_circle');
+
+const degree = 3.6 * (100 - parseInt(donut.getAttribute('data-per')));
+const r = parseInt(donut.getAttribute('r'));
+
+// document.documentElement.style.setProperty('--l', r * 3.141592 * 2);
+// document.documentElement.style.setProperty('--deg', degree.toString() + "deg");
+// document.documentElement.style.setProperty('--result', 2 * 3.141592 * r * (degree / 360));
+
+console.log( r * 3.141592 * 2); //157.0796
+donut.style.strokeDasharray = r * 3.141592 * 2;
+donut.style.strokeDashoffset = r * 3.141592 * 2;
+// donut.style.strokeDashoffset = -140;
+
+let i = 0
+const loading = setInterval(() => {
+
+    if (i <= -157) {
+        i = -157;
+    } else { i--; }
+
+    donut.style.strokeDashoffset = (r * 3.141592 * 2) - i;
+    
+}, 100);
