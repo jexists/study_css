40042a4f489d5dd555031b9608e4cd4a5587c5d1
jexists <jexists486@gmail.com>
2021-11-11 22:01:51 +0900

    facebook done

---
+++ facebook/index.js
@@ -0,0 +1,18 @@
+
+console.log('????');
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+
+function SetActive(i) {
+  for (slide of slides){
+  slide.classList.remove('active');
+  slides[i].classList.add('active');}
+  for (dot of dots){
+  dot.classList.remove('active');
+  dots[i].classList.add('active');}
+}
+for (let j = 0; j < dots.length; j++) {
+  dots[j].addEventListener('click', function () {
+    SetActive(j);
+  })
+}
