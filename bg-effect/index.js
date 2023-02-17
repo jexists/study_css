e08624fa0c735be6274385c67f7b309cae0b710e
jexists <jexists486@gmail.com>
2023-02-17 22:36:23 +0900

    랜덤 생성

--- bg-effect/index.js
+++ bg-effect/index.js
@@ -1,1 +1,12 @@
 console.log('스크립트 연결 완료!')
+
+const section = document.querySelector('.section')
+// console.error(section);
+
+for (let i = 0; i < 400; i++) {
+  const box = document.createElement('i')
+  // <i style="--i:11;"></i>
+  // console.error(Math.floor(Math.random() * 20));
+  box.style.cssText = `--i:${Math.floor(Math.random() * 20)}`;
+  section.append(box);
+}
